5647f65746e29d658ba07ada633036ff020e8699
hilda1737 <hildamoraahh@gmail.com>
2022-03-01 09:28:43 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val x = "my name is "
+    val name = "mary"
+    println("$x name")
+
+
+var text = "Hilda"
+    println(text[4])
+
+    var year1=2022
+    var year2=2001
+    var t= year1-year2
+    println(t)
+
+    val you: Int = 789
+    val nai: Int = 798
+    if(you==nai)
+        else{
+
+        }
+    println(true)
+}
